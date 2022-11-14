f25a24ff83f01f3b2934b0e74493db899c12c870
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-11-14 18:06:59 +0200

    lab_5

---
+++ lab_5/lab_5/lab_5.cpp
@@ -0,0 +1,26 @@
+﻿// Створення вказівника, виділення пам'яті для змінної a,
+// розіменування і присвоєння змінній a значення, присвоєння 
+// b значення а, знищення пам'яті виділеної під покажчик b
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int *a;
+
+	a = new int;
+
+	*a = 25;
+
+	int b = *a;
+
+	delete a;
+
+	cout << b;
+}
